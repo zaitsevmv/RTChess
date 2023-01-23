a37d078b2f7a291a71dbcf2aeb2e8c4a49936348
Matvey <qazsewpp@gmail.com>
2023-01-23 21:09:41 +0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <QApplication>
+
+#include "Visuals/Frontend.h"
+
+int main(int argc, char *argv[]) {
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.RunGameMenu();
+    w.show();
+
+    //MainWindow w;
+    //w.ForTests = true;
+    //w.RunGameMenu();
+    //w.show();
+    //w.testWidget->show();
+    //пример приложения для тестов
+
+    //MainWindow w1;
+    //w1.RunGameMenu();
+    //w1.show();
+    //пример стандартного приложения
+    return QApplication::exec();
+}
